af4b3a9617f7832a560c9032fe8fa784a09d2a30
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-23 13:32:29 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+  return (
+    <div className="px-12">
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;

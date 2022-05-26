f0b4f49595e27fd47d6434899d66726c47af25db
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-26 14:00:09 +0600

    add product

--- src/hook/useDrillProduct.js
+++ src/hook/useDrillProduct.js
@@ -3,12 +3,7 @@ import { useEffect, useState } from "react";
 const useDrillProduct = () => {
   const [tools, setTools] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/product", {
-      method: "GET",
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      },
-    })
+    fetch("http://localhost:5000/product")
       .then((res) => res.json())
       .then((data) => setTools(data));
   }, []);
